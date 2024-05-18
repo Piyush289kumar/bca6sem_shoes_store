d82a2e524b6374163582ecb4b078a1b9da38a58d
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-18 22:36:34 +0530

    "Updated hostname in config.php and added duplicate font-awesome.css link in header.php"

--- admin/config.php
+++ admin/config.php
@@ -1,4 +1,4 @@
 <?php
 $website_display_default_name = "DJ Rishab";
-$hostname = "http://localhost/rishabtiwariPortfolio";
+$hostname = "http://localhost/shoes_store";
 $conn = mysqli_connect('localhost', 'root', '', 'dj_rishab') or die("Connection Failed!!" . mysqli_connect_error());
--- admin/header.php
+++ admin/header.php
@@ -66,6 +66,7 @@ switch ($current_page) {
     <!-- Custom stlylesheet -->
     <link rel="stylesheet" href="../css/bootstrap.minDJ.css" />
     <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/font-awesome.css">
     <link rel="stylesheet" href="../css/adminstyle_center.css">
     <link rel="shortcut icon" type="x-con" href="../img/music.png">
     <link rel="icon" href="../img/music.png" />
