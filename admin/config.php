9edc49f1cc5c07ee933647c0c0ba3ac39a056fb5
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-18 22:36:41 +0530

    "Updated website display default name and database connection settings."

--- admin/config.php
+++ admin/config.php
@@ -1,4 +1,4 @@
 <?php
-$website_display_default_name = "DJ Rishab";
+$website_display_default_name = "Shoes Store";
 $hostname = "http://localhost/shoes_store";
-$conn = mysqli_connect('localhost', 'root', '', 'dj_rishab') or die("Connection Failed!!" . mysqli_connect_error());
+$conn = mysqli_connect('localhost', 'root', '', 'shoes_store') or die("Connection Failed!!" . mysqli_connect_error());
