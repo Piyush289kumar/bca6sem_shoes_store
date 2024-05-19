b0aebca03bd00fa0fc998f118a435042ac1f6960
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-19 21:21:48 +0530

    Footer Build

--- footer.php
+++ footer.php
@@ -1,19 +1,19 @@
-
 <!-- start footer Area -->
 <footer class="footer-area section_gap">
 	<div class="container">
 		<div class="row">
-			<div class="col-lg-3  col-md-6 col-sm-6">
+			<div class="col-lg-4  col-md-6 col-sm-6">
 				<div class="single-footer-widget">
 					<h6>About Us</h6>
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-						labore dolore
-						magna aliqua.
+						Elevate Your Style with Our Exclusive Collection
+						Welcome to [Your Store Name], where fashion meets comfort and quality. Our meticulously curated
+						collection of shoes is designed to cater to all your footwear needs, whether you're looking for
+						the latest trends, timeless classics, or everyday essentials.
 					</p>
 				</div>
 			</div>
-			<div class="col-lg-4  col-md-6 col-sm-6">
+			<div class="col-lg-6  col-md-6 col-sm-6">
 				<div class="single-footer-widget">
 					<h6>Newsletter</h6>
 					<p>Stay update with our latest</p>
@@ -30,46 +30,27 @@
 									required="" type="email">
 
 
-								<button class="click-btn btn btn-default"><i class="fa fa-long-arrow-right"
-										aria-hidden="true"></i></button>
+								<a href="mailto:piyushraikwar@example.com" class="click-btn btn btn-default"><i
+										class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
 								<div style="position: absolute; left: -5000px;">
 									<input name="b_36c4fd991d266f23781ded980_aefe40901a" tabindex="-1" value=""
 										type="text">
 								</div>
 
-								<!-- <div class="col-lg-4 col-md-4">
-												<button class="bb-btn btn"><span class="lnr lnr-arrow-right"></span></button>
-											</div>  -->
 							</div>
 							<div class="info"></div>
 						</form>
 					</div>
 				</div>
 			</div>
-			<div class="col-lg-3  col-md-6 col-sm-6">
-				<div class="single-footer-widget mail-chimp">
-					<h6 class="mb-20">Instragram Feed</h6>
-					<ul class="instafeed d-flex flex-wrap">
-						<li><img src="img/i1.jpg" alt=""></li>
-						<li><img src="img/i2.jpg" alt=""></li>
-						<li><img src="img/i3.jpg" alt=""></li>
-						<li><img src="img/i4.jpg" alt=""></li>
-						<li><img src="img/i5.jpg" alt=""></li>
-						<li><img src="img/i6.jpg" alt=""></li>
-						<li><img src="img/i7.jpg" alt=""></li>
-						<li><img src="img/i8.jpg" alt=""></li>
-					</ul>
-				</div>
-			</div>
-			<div class="col-lg-2 col-md-6 col-sm-6">
+
+			<div class="col-lg-2 col-md-12 col-sm-12">
 				<div class="single-footer-widget">
 					<h6>Follow Us</h6>
 					<p>Let us be social</p>
 					<div class="footer-social d-flex align-items-center">
 						<a href="#"><i class="fa fa-facebook"></i></a>
 						<a href="#"><i class="fa fa-twitter"></i></a>
-						<a href="#"><i class="fa fa-dribbble"></i></a>
-						<a href="#"><i class="fa fa-behance"></i></a>
 					</div>
 				</div>
 			</div>
@@ -78,9 +59,9 @@
 			<p class="footer-text m-0">
 				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 				Copyright &copy;
-				<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made
-				with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com"
-					target="_blank">Colorlib</a>
+				<script>document.write(new Date().getFullYear());</script> All rights reserved | <i
+					class="fa fa-heart-o" aria-hidden="true"></i>
+				<a>Manas</a>
 				<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
 			</p>
 		</div>
