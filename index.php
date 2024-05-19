a206fb677b304b37015161a01f28fd29b51a82ce
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-19 21:16:47 +0530

    "Updated CSS to add WhatsApp icon styles, removed redundant HTML code in index.php, and added PHP code to display products with pagination"

--- index.php
+++ index.php
@@ -82,7 +82,6 @@
 				</div>
 			</div>
 			<!-- single features -->
-
 		</div>
 	</div>
 </section>
@@ -155,7 +154,6 @@
 </section>
 <!-- End category Area -->
 <!-- start product Area -->
-
 <div class="container mt-5">
 	<div class="row justify-content-center">
 		<div class="col-lg-6 text-center">
@@ -165,270 +163,46 @@
 		</div>
 	</div>
 	<div class="row">
-
-	
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p1.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- single product -->
-
-
-
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p2.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
+		<!-- PHP CODE -->
+		<?php
+		include ("config.php");
+		if (isset($_GET['page_num_index'])) {
+			$page_num_index_by_addbar = $_GET['page_num_index'];
+		} else {
+			$page_num_index_by_addbar = 1;
+		}
+		$record_limit = 10;
+		$offset = ($page_num_index_by_addbar - 1) * $record_limit;
+		$sql_show_user = "SELECT * FROM achievement WHERE active_record = 'Yes' ORDER BY aid DESC LIMIT {$offset},{$record_limit}";
+		$result_sql_show_user = mysqli_query($conn, $sql_show_user) or die("Query Failed!!");
+		if (mysqli_num_rows($result_sql_show_user) > 0) {
+			$serial_num = $offset + 1;
+			while ($row = mysqli_fetch_assoc($result_sql_show_user)) {
+				?>
+				<!-- single product -->
+				<div class="col-lg-3 col-md-6">
+					<div class="single-product">
+						<img class="img-fluid" src="admin/upload/<?php echo ($row['aimg']) ?>" alt="Error">
+						<div class="product-details">
+							<h6><?php echo ($row['atitle']) ?></h6>
+							<div class="price">
+								<h6>₹ <?php echo ($row['atype']) ?></h6>
+							</div>
+							<div class="prd-bottom">
+								<a href="whatsapp:contact=+918817762774@s.whatsapp.com&message=I'd like to chat with you">
+									<img src="img/whatsapppng.png" alt="Whats App" class="whatappIcon">
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</div>
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p3.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p4.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p5.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p6.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p7.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
-		<!-- single product -->
-		<div class="col-lg-3 col-md-6">
-			<div class="single-product">
-				<img class="img-fluid" src="img/product/p8.jpg" alt="">
-				<div class="product-details">
-					<h6>addidas New Hammer sole
-						for Sports person</h6>
-					<div class="price">
-						<h6>$150.00</h6>
-						<h6 class="l-through">$210.00</h6>
-					</div>
-					<div class="prd-bottom">
-						<a href="" class="social-info">
-							<span class="ti-bag"></span>
-							<p class="hover-text">add to bag</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-heart"></span>
-							<p class="hover-text">Wishlist</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-sync"></span>
-							<p class="hover-text">compare</p>
-						</a>
-						<a href="" class="social-info">
-							<span class="lnr lnr-move"></span>
-							<p class="hover-text">view more</p>
-						</a>
-					</div>
-				</div>
-			</div>
-		</div>
+				<!-- single product -->
+			<?php }
+		} ?>
 	</div>
 </div>
-
+</div>
+</div>
 <!-- End exclusive deal Area -->
 <!-- Start brand Area -->
 <section class="brand-area section_gap">
@@ -453,141 +227,4 @@
 	</div>
 </section>
 <!-- End brand Area -->
-<!-- Start related-product Area -->
-<section class="related-product-area section_gap_bottom">
-	<div class="container">
-		<div class="row justify-content-center">
-			<div class="col-lg-6 text-center">
-				<div class="section-title">
-					<h1>Deals of the Week</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-						labore et dolore
-						magna aliqua.</p>
-				</div>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-lg-9">
-				<div class="row">
-					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r1.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r2.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r3.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r5.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r6.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6 mb-20">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r7.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r9.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r10.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-4 col-sm-6">
-						<div class="single-related-product d-flex">
-							<a href="#"><img src="img/r11.jpg" alt=""></a>
-							<div class="desc">
-								<a href="#" class="title">Black lace Heels</a>
-								<div class="price">
-									<h6>$189.00</h6>
-									<h6 class="l-through">$210.00</h6>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="col-lg-3">
-				<div class="ctg-right">
-					<a href="#" target="_blank">
-						<img class="img-fluid d-block mx-auto" src="img/category/c5.jpg" alt="">
-					</a>
-				</div>
-			</div>
-		</div>
-	</div>
-</section>
-<!-- End related-product Area -->
 <?php include ('footer.php') ?>
