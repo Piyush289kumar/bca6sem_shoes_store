073733552dd2e9bf3ca98f8caf07dd44b1c04581
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-20 21:22:10 +0530

    "Added commented out alternative configuration settings for website name, hostname, and database connection."

--- admin/config.php
+++ admin/config.php
@@ -2,3 +2,8 @@
 $website_display_default_name = "Shoes Store";
 $hostname = "http://localhost/shoes_store";
 $conn = mysqli_connect('localhost', 'root', '', 'shoes_store') or die("Connection Failed!!" . mysqli_connect_error());
+
+
+// $website_display_default_name = "Shoes Store";
+// $hostname = "https://cibadress.com/app/bca6sem/shoes_store";
+// $conn = mysqli_connect('localhost', 'u423777452_shoes_store', 'Piyush@289', 'u423777452_shoes_store') or die("Connection Failed!!" . mysqli_connect_error());
