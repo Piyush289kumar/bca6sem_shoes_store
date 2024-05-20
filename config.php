5464fb780fe3e72bfbb2ec2fa95b0c4bcbf9e724
Piyush289kumar <piyushraikwar289@gmail.com>
2024-05-20 21:21:38 +0530

    config update

--- config.php
+++ config.php
@@ -6,4 +6,4 @@ $conn = mysqli_connect('localhost', 'root', '', 'shoes_store') or die("Connectio
 
 // $website_display_default_name = "Shoes Store";
 // $hostname = "https://cibadress.com/app/bca6sem/shoes_store";
-// $conn = mysqli_connect('localhost', 'root', 'Piyush@289', 'shoes_store') or die("Connection Failed!!" . mysqli_connect_error());
+// $conn = mysqli_connect('localhost', 'u423777452_shoes_store', 'Piyush@289', 'u423777452_shoes_store') or die("Connection Failed!!" . mysqli_connect_error());
