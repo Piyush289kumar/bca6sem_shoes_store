101fb8267c93674cb813d784f3e9411a495a50d2
Piyush Kumar Raikwar <94155141+Piyush289kumar@users.noreply.github.com>
2024-05-31 10:04:00 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -32,12 +32,12 @@
 					</div>
 					<div class="info_item">
 						<i class="lnr lnr-phone-handset"></i>
-						<h6><a href="#">00 (440) 9865 562</a></h6>
+						<h6><a>+91 76111-87538</a></h6>
 						<p>Mon to Fri 9am to 6 pm</p>
 					</div>
 					<div class="info_item">
 						<i class="lnr lnr-envelope"></i>
-						<h6><a href="#">support@colorlib.com</a></h6>
+						<h6><a>sonimanas60@gmail.com</a></h6>
 						<p>Send us your query anytime!</p>
 					</div>
 				</div>
